668383ef3a42d7203516bffaaf1186d02aa7ef7a
Nate H <NSHenry@users.noreply.github.com>
2023-07-27 13:55:13 -0400

    Turn off tap dance, combos, and overrides.
    Four BT devices plus these features lead to buffer overflows when typing at speed. (which for me is slow)

--- keyboards/keychron/q1_pro/ansi_knob/keymaps/vial/rules.mk
+++ keyboards/keychron/q1_pro/ansi_knob/keymaps/vial/rules.mk
@@ -2,5 +2,10 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 
+# Turning off Tap Dance, Dynamic Combos, and Key Overrides in an attempt to allow for 6 device capability without buffer overruns.
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
 # LTO to shrink overall firmware size. Testing on Surface - nhenry 7-27-23
 LTO_ENABLE = yes
